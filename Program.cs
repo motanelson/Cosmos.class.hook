056a6d88ed2f36c03f9e280b773b113e0fd7fdf8
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-30 20:57:41 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 public class x
 {
+    private static string spaces(int a) 
+    {
+        string b = "";
+        for (int i = 0; i < a; i++) b = b + " ";
+        return b;
+    }
     public static void box(int x,int y,int w,int h)
     {
-        Console.WriteLine("x:{0}\ny:{1}\nw:{2}\nh:{3}\n",x,y,w,h);
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+
+        for (int i = 0; i < y/8; i++) Console.WriteLine("");
+        
+        for (int i = 0; i < h/8; i++)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(spaces(x/8));
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(spaces(w/8));Console.WriteLine("");
+        }
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
     }
 }
 class xclass {
     public static void Main() {
-        Console.BackgroundColor = ConsoleColor.White;
-        Console.ForegroundColor = ConsoleColor.Black;
+        
         Console.Clear();
-        x.box(0,0,10,10);
+        x.box(16,16,80,80);
     }
 }
