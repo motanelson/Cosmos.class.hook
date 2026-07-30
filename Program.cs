b5cf9b993585f0fcae44db63ada192bca898dff0
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-30 18:59:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class x
+{
+    public static void box(int x,int y,int w,int h)
+    {
+        Console.WriteLine("x:{0}\ny:{1}\nw:{2}\nh:{3}\n",x,y,w,h);
+    }
+}
+class xclass {
+    public static void Main() {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        x.box(0,0,10,10);
+    }
+}
